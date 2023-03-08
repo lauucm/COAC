01dfe277b000bd04d8d245e3ecc6a49852a34a12
Laura Cabrera <alum.lcabreram.2022@iesalixar.org>
2023-03-08 20:51:31 +0100

    Documentación de la clase Agrupación

--- src/model/Agrupacion.java
+++ src/model/Agrupacion.java
@@ -1,14 +1,36 @@
 package model;
 
+/**
+ * <h2>Clase Agrupación, se utiliza para crear la agrupacion general</h2>
+ * @author laura
+ * @since 08/03/2023
+ */
+
 public abstract class Agrupacion implements Comparable <Agrupacion> {
 
-	
+	/**
+	 * Atributo nombre de la agrupación
+	 */
 	private String nombre; 
+	/**
+	 * Atributo autor de la agrupación
+	 */
 	private String autor; 
+	/**
+	 * Atributo autor de la música de la agrupación
+	 */
 	private String autor_musica; 
+	/**
+	 * Atributo autor de las letras de la agrupación
+	 */
 	private String autor_letras; 
+	/**
+	 * Atributo tipo de disfraz de la agrupación
+	 */
 	private String disfraz; 
-	
+	/**
+	 * constructor por defecto de la clase
+	 */
 	public Agrupacion() {
 		
 	}
@@ -52,17 +74,35 @@ public abstract class Agrupacion implements Comparable <Agrupacion> {
 	public void setDisfraz(String disfraz) {
 		this.disfraz = disfraz;
 	}
-	
+	/**
+	 * método abstracto, la implementacion se realizará en las clases hijas
+	 * @see Chirigota
+	 * @see Comparsa
+	 * @see Cuarteto
+	 * @see Romancero
+	 */
 	public abstract String cantarPresentacion();
-	
+	/**
+	 * método abstracto, la implementación se realizará en las clases hijas
+	 * @see Chirigota
+	 * @see Comparsa
+	 * @see Cuarteto
+	 * @see Romancero
+	 */
 	public abstract String hacerTipo(); 
 	
+	/**
+	 * Para devolver en una cadena de texto los objetos de la clase 
+	 * @return objetos de la agrupación
+	 */
 	@Override
 	public String toString() {
 		return "Nombre: " + getNombre() + ", Autor: " + getAutor() + ", Autor de Música: " + getAutor_musica()
 				+ ", Autor de Letras: " + getAutor_letras() + ", Tipo: " + getDisfraz();
 	}
-	
+	/**
+	 * método para comparar los nombres de las agrupaciones
+	 */
 	@Override
 	public int compareTo (Agrupacion a) {
 		return this.getNombre().compareTo(a.getNombre());
