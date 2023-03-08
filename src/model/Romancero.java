5eb5a10dc5e6616fca319caf520c8acbec3af7c7
Laura Cabrera <alum.lcabreram.2022@iesalixar.org>
2023-03-08 21:42:00 +0100

    Documentacion romancero

--- src/model/Romancero.java
+++ src/model/Romancero.java
@@ -1,12 +1,24 @@
 package model;
 
-public class Romancero extends Agrupacion implements Callejera{
+/**
+ * <h2>Clase Romancero, un tipo de las agrupaciones</h2>
+ * 
+ * @author laura
+ * @since 08/03/2023
+ */
 
-	
-	private String tematica; 
-	
+public class Romancero extends Agrupacion implements Callejera {
+
+	/**
+	 * atributo temática del romancero
+	 */
+	private String tematica;
+
+	/**
+	 * metodo constructor por defecto
+	 */
 	public Romancero() {
-		
+
 	}
 
 	public String getTematica() {
@@ -17,20 +29,40 @@ public class Romancero extends Agrupacion implements Callejera{
 		this.tematica = tematica;
 	}
 
+	/**
+	 * indica que se esta cantando la presentación de el romancero
+	 * 
+	 * @return mensaje de especificación
+	 */
 	@Override
 	public String cantarPresentacion() {
 		return "Cantando la presentación del Romancero " + super.getNombre();
 	}
-
+	/**
+	 * indica que tipo de disfraz lleva el romancero
+	 * 
+	 * @return mensaje de especificación
+	 */
 	@Override
 	public String hacerTipo() {
 		return "El romancer " + super.getNombre() + " va de " + super.getDisfraz();
 	}
+	/**
+	 * indica que Romancero se va a escuchar
+	 * 
+	 * @return mensaje de especificación
+	 */
 
 	@Override
 	public String amoAescucha() {
 		return "Amo a escucha el romancero " + super.getNombre();
 	}
+
+	@Override
+	public String toString() {
+		return "Romancero: tematica= " + tematica + super.toString();
+	}
 	
 	
+
 }
