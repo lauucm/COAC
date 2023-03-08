bbd70de7157ceb34ceee605626fb5b33106d6d9d
Laura Cabrera <alum.lcabreram.2022@iesalixar.org>
2023-03-08 21:22:55 +0100

    Documentacion

--- src/model/Callejera.java
+++ src/model/Callejera.java
@@ -1,6 +1,22 @@
 package model;
 
+/**
+ * <h2>Interfaz Callejera</h2>
+ * 
+ * @author laura
+ * @since 08/03/2023
+ */
+
 public interface Callejera {
 
+	/**
+	 * metodo donde suya implementación se realizará en las diversas clases donde se
+	 * implemente la interfaz
+	 * 
+	 * @see Cuarteto
+	 * @see Chirigota
+	 * @see Romancero
+	 * @see Comparsa
+	 */
 	public String amoAescucha();
 }
