e860c3368471cdc287863e785f27004f98f62ee1
Laura Cabrera <alum.lcabreram.2022@iesalixar.org>
2023-03-09 13:39:55 +0100

    Documentacion integrante

--- src/model/Integrante.java
+++ src/model/Integrante.java
@@ -1,15 +1,34 @@
 package model;
 
+/**
+ * <h2>Clase Cuarteto, un tipo de las agrupaciones oficiales</h2>
+ * 
+ * @author laura
+ * @since 09/03/2023
+ */
 public class Integrante {
+	/**
+	 * atributo estático numero de participantes de integrantes
+	 */
+	private static Integer numeroParticipante;
+	/**
+	 * atributo nombre del integrante
+	 */
+	private String nombre;
+	/**
+	 * atributo edad del integrante
+	 */
+	private Integer edad;
+	/**
+	 * atributo localidad del integrante
+	 */
+	private String localidad;
 
-	
-	private static Integer numeroParticipante; 
-	private String nombre; 
-	private Integer edad; 
-	private String localidad; 
-	
-	public Integrante () {
-		
+	/**
+	 * metodo constructor por defecto
+	 */
+	public Integrante() {
+
 	}
 
 	public static Integer getNumeroParticipante() {
@@ -43,6 +62,16 @@ public class Integrante {
 	public void setLocalidad(String localidad) {
 		this.localidad = localidad;
 	}
-	
-	
+
+	/**
+	 * método para devolver una cadena de texto de los objetos de la clase
+	 * integrante
+	 * 
+	 * @return objetos de esta clase
+	 */
+	@Override
+	public String toString() {
+		return "Integrante: Nombre= " + getNombre() + ", Edad= " + getEdad() + ", Localidad= " + getLocalidad();
+	}
+
 }
