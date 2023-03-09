6750d1e0daa942e9f4ccab23f020b451081666bd
Laura Cabrera <alum.lcabreram.2022@iesalixar.org>
2023-03-09 14:09:04 +0100

    Documentación sort_puntos

--- src/model/Sort_Puntos.java
+++ src/model/Sort_Puntos.java
@@ -2,14 +2,34 @@ package model;
 
 import java.util.Comparator;
 
-public class Sort_Puntos implements Comparator <Agrupacion>{
-
+/**
+ * <h2>Clase sort_puntos, usada para comparar los puntos de las agrupaciones
+ * oficiales</h2>
+ * 
+ * @author laura
+ * @since 08/03/2023
+ */
+public class Sort_Puntos implements Comparator<Agrupacion> {
+	/**
+	 * metodo de comparacion, cuyo objetivo es comparar los nombres de las
+	 * agrupaciones
+	 * 
+	 * @return
+	 *         <ul>
+	 *         <li>0: los puntos son iguales
+	 *         <li>
+	 *         <li>1: puntos o1 es mayor que o2
+	 *         <li>
+	 *         <li>-1: puntos o1 menor que o2
+	 *         <li>
+	 *         </ul>
+	 */
 	@Override
 	public int compare(Agrupacion o1, Agrupacion o2) {
-		int comp=0;
-		if (o1!=null && o2!= null) {
+		int comp = 0;
+		if (o1 != null && o2 != null) {
 			if (o1 instanceof AgrupacionOficial && o2 instanceof AgrupacionOficial) {
-				comp= ((AgrupacionOficial)o1).getPuntos().compareTo(((AgrupacionOficial)o2).getPuntos());
+				comp = ((AgrupacionOficial) o1).getPuntos().compareTo(((AgrupacionOficial) o2).getPuntos());
 			}
 		}
 		return comp;
